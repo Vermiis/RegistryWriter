5342a5594ea968607f902a5baac3a8ae65a730e5
Nira <Nira@Vermiis>
2017-05-16 06:04:51 +0200

    .

--- RegistryWriter/ReadWrite.cs
+++ RegistryWriter/ReadWrite.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace RegistryWriter
 {
-    class ReadWrite
+    public class ReadWrite
     {
+        public static void ShowValue(string key, string valname)
+        {          
+            Console.WriteLine(Registry.GetValue(key, valname, "NotFound"));
+        }
+
+        public static void ChangeValue (string key, string valname, string val )
+        {
+            Registry.SetValue(key, valname, val);
+        }
+
     }
 }
